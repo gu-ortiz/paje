7f19b5301c7a2e6c11d14b85bb1a794f6a7afefb
Gustavo Ortiz <gustavo.egert.ortiz@gmail.com>
2023-04-25 19:38:17 -0300

    adding logo & finishing searchbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 'use client';
+import logo from 'assets/logo.svg';
 import { Search } from 'lucide-react';
+import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 
 const Header = () => {
@@ -14,19 +16,31 @@ const Header = () => {
   return (
     <header className="w-full h-20">
       <div className="w-full h-full flex justify-center items-center border-b border-gray-200 shadow-sm">
-        <div className="container flex mx-auto justify-center">
-          <div className="relative w-1/2">
-            <input
-              type="text"
-              placeholder="Pesquisar termos..."
-              value={searchQuery}
-              onChange={(e) => handleSearch(e)}
-              className="w-full h-10 py-2 pl-5 pr-12 leading-tight text-gray-300 bg-white border border-gray-300 rounded-full shadow-sm focus:text-gray-700 focus:border-blue-500 focus:outline-none placeholder:text-gray-300"
+        <div className="container h-full flex mx-auto justify-between">
+          <div className="relative h-full flex justify-center items-center">
+            <Image
+              src={logo}
+              alt="Apollo"
+              width={40}
+              height={40}
+              className="flex justify-center items-center object-contain"
             />
-            <div className="absolute inset-y-0 right-1 w-12 h-full flex justify-center items-center">
-              <Search className="text-gray-300" size={18} />
+          </div>
+          <div className="relative w-1/2 h-full flex justify-center items-center">
+            <div className="w-full flex group border-0 rounded-full">
+              <input
+                type="text"
+                placeholder="Pesquisar termos..."
+                value={searchQuery}
+                onChange={(e) => handleSearch(e)}
+                className="w-full h-10 pl-5 pr-12 py-2 shadow-sm border rounded-l-full border-gray-300 text-gray-300  bg-white placeholder:text-gray-300 group-hover:border-blue-500 focus:outline-none focus:border-blue-500 focus:text-gray-600"
+              />
+              <button className="relative w-12 h-10 pr-px flex justify-center items-center shadow-sm border border-l-0 rounded-r-full border-gray-300 text-gray-300 bg-white hover:border-blue-500 hover:text-blue-500 active:text-white active:bg-blue-500">
+                <Search size={18} />
+              </button>
             </div>
           </div>
+          <div className="relative flex"></div>
         </div>
       </div>
     </header>
