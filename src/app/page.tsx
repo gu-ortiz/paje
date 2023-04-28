78613b77413c9032d0d6f0c63c637ad9077939bf
Gustavo Ortiz <gustavo.egert.ortiz@gmail.com>
2023-04-27 22:59:25 -0300

    responsive header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main className="flex">oi</main>;
+  return <main className="flex"> </main>;
 }
--- src/components/HeaderSearch.tsx
+++ src/components/HeaderSearch.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Search } from 'lucide-react';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { ChangeEvent, useState } from 'react';
 
 const HeaderSearch = () => {
@@ -22,7 +22,7 @@ const HeaderSearch = () => {
           className="w-full h-10 pl-5 pr-12 py-2 shadow-sm border rounded-l-full border-gray-300 text-gray-300  bg-white placeholder:text-gray-300 group-hover:border-blue-500 focus:outline-none focus:border-blue-500 focus:text-gray-600"
         />
         <button className="relative w-12 h-10 pr-px flex justify-center items-center shadow-sm border border-l-0 rounded-r-full border-gray-300 text-gray-300 bg-white hover:border-blue-500 hover:text-blue-500 active:text-white active:bg-blue-500">
-          <Search size={18} />
+          <MagnifyingGlassIcon className="block h-5 w-5" aria-hidden="true" />
         </button>
       </div>
     </div>
