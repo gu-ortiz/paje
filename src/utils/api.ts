6b82546db156861271f4d758607abd4a86989dfb
Gustavo Ortiz <gustavo.egert.ortiz@gmail.com>
2023-06-26 19:14:57 -0300

    fix anvisa revalidation

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -36,7 +36,7 @@ export async function getTerm(url: string): Promise<ResponseTermType> {
 }
 
 export async function getAnvisa(url: string): Promise<ResponseAnvisaType> {
-  const response = await fetch(url, { next: { revalidate: 0 } });
+  const response = await fetch(url, { next: { revalidate: 600 } });
 
   if (!response.ok)
     return {
